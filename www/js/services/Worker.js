6f3a4f27b5ad1a1c2aa730bd8a17bb2c88b2bb9b
Amilson Junior <amilson_junior@outlook.com>
2016-01-21 16:27:47 -0200

    Update Worker.js

--- www/js/services/Worker.js
+++ www/js/services/Worker.js
@@ -2,28 +2,28 @@ var app = angular.module('freevice');
 
 app.factory('Worker', function($firebase, $state, $ionicPopup){
   var ref = new Firebase('https://desk-solution.firebaseio.com/users');
-  var tecnicos = [];
-  var selectedTecnico = {};
+  var workers = [];
+  var selectedWorker = {};
   
   return{
-    setTecnicos: function(data){ tecnicos = data; },
+    setTecnicos: function(data){ workers = data; },
       getTecnicos: function(){
-        tecnicos = [];
+        workers = [];
         ref.on('value', function(data){
           var _tecnicos = data.val();
 
           for(var x in _tecnicos){
             if(x.tecnico)
-              tecnicos.push(_tecnicos[x]);
+              workers.push(_tecnicos[x]);
           }
 
-          console.log(tecnicos);
+          console.log(workers);
         }, function(error){
           console.log('Failed to read data: ' + error);
         });
-        return tecnicos;
+        return workers;
       },
-      selectWorker: function(tecnico){ selectedTecnico = tecnico; },
+      selectWorker: function(worker){ selectedWorker = worker; },
       getDataById: function(id){
         var facebookID = 'facebook:' + id;
 
@@ -33,7 +33,6 @@ app.factory('Worker', function($firebase, $state, $ionicPopup){
            return data.name;
         });
       },
-      getSelectedWorker: function(){ return selectedTecnico; },
-      Tecnicos: tecnicos
+      getSelectedWorker: function(){ return selectedTecnico; }
   }
 });
